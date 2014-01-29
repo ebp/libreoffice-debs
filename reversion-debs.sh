521a0a649b3a8ab7f20d1be444b74f195fe4accb
root <root@util01.dal0.ebackpack.org>
2014-01-29 19:21:11 +0000

    Bump to fake version 4.3.0.1 for deb packaging.

--- reversion-debs.sh
+++ reversion-debs.sh
@@ -4,14 +4,14 @@
 # Destroys alpha1-1 deb files in debs/ after complete.
 #
 for deb in debs/*.deb; do
-	new=$(echo $deb | perl -pe 's(alpha1\-1)(alpha1)')
+	new=$(echo $deb | perl -pe 's(alpha0\-1)(alpha0)')
 	dir="$(basename $deb .deb)"
 	mkdir -p "$dir"
 	dpkg-deb -x "$deb" "$dir"
 	dpkg-deb --control "$deb" "$dir/DEBIAN"
-	perl -i -pe 's(alpha1\-1)(alpha1)g' "$dir/DEBIAN/control"
+	perl -i -pe 's(alpha0\-1)(alpha0)g' "$dir/DEBIAN/control"
 	dpkg -b "$dir" "$new"
 	rm -fr "$dir"
 done
 
-rm -f debs/*alpha1-1*.deb
+rm -f debs/*alpha0-1*.deb
